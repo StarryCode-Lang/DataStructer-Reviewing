3b8bf911ed72a942a41bb07cbd7cbe56fc07c5c5
Starry Code <110176785+StarryCode-Lang@users.noreply.github.com>
2025-05-12 20:27:55 +0800

    05121901

--- 6-5TopologicalSort.cpp
+++ 6-5TopologicalSort.cpp
@@ -1,3 +1,42 @@
 #include <iostream>
+#include <stack>
 #include "6-0GraphDefinition.h"
+
 using namespace std;
+
+void FindInDegree(const ALGraph &G, vector<int> &inDegree) {
+    inDegree.resize(G.vexnum, 0);
+    for (int i = 0; i < G.vexnum; i++) {
+        for (ArcNode *p = G.vertices[i].firstarc; p; p = p->nextarc) {
+            inDegree[p->adjvex]++;
+        }
+    }
+}
+
+bool TopologicalSort(const ALGraph &G) {
+    vector<int> inDegree(G.vexnum, 0);
+    FindInDegree(G, inDegree);
+    stack<int> S;
+    int i;
+    int count = 0; // 输出顶点计数
+    for (i = 0; i < G.vexnum; ++i) {
+        if (inDegree[i] == 0) {
+            S.push(i);
+        }
+    }
+    cout << "序列" << endl;
+    while (!S.empty()) {
+        int v = S.top();
+        S.pop();
+        cout << G.vertices[i].data << " ";
+        count++;
+
+        for (ArcNode *p = G.vertices[v].firstarc; p; p = p->nextarc) {
+            if (!(--inDegree[p->adjvex])) {
+                S.push(p->adjvex);
+            }
+        }
+    }
+    cout << endl;
+    return count == G.vexnum;
+}
